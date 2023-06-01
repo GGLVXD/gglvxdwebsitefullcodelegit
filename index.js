e2e0924c3d29ca9149f29123876e84738652e7e5
GGLVXD <85647114+GGLVXD@users.noreply.github.com>
2023-06-01 14:40:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-async function fetchRoutes() {
-  try {
-    const response = await axios.get('http://n7.danbot.host:1629/routes'); // Replace with the correct API endpoint to fetch routes
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching routes:', error);
-    return [];
-  }
-}
+const routes = [
+  '/',
+  '/friends/potatolutz.html',
+  '/endercraft',
+  '/sitemaps.xml',
+  '/robots.txt',
+  '/css',
+  '/img',
+  '/googlecba8dce2c35e487c.html',
+  '/maxcraft',
+  '/minecraft',
+  '/skins'
+];
 
-async function generateIframeRoutes() {
-  const routes = await fetchRoutes();
+const IframeRoute = ({ route }) => {
+  return (
+    <iframe
+      src={`http://n7.danbot.host:1629${route}`}
+      title={route}
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+};
 
-  if (routes.length === 0) {
-    console.warn('No routes found.');
-    return;
-  }
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route key={index} exact path={route}>
+            <IframeRoute route={route} />
+          </Route>
+        ))}
+      </Switch>
+    </Router>
+  );
+};
 
-  const iframeTemplate = (route) => `
-    <html>
-      <head>
-        <title>${route}</title>
-        <style>
-          body, html {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            overflow: hidden;
-          }
-          iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-          }
-        </style>
-      </head>
-      <body>
-        <iframe src="http://n7.danbot.host:1629${route}"></iframe>
-      </body>
-    </html>
-  `;
-
-  routes.forEach(async (route) => {
-    const iframeContent = iframeTemplate(route);
-    const filePath = path.join('build', 'pages', route, 'index.html');
-    const directoryPath = path.dirname(filePath);
-
-    if (!fs.existsSync(directoryPath)) {
-      fs.mkdirSync(directoryPath, { recursive: true });
-    }
-
-    fs.writeFileSync(filePath, iframeContent);
-    console.log(`Generated ${route}`);
-  });
-}
-
-generateIframeRoutes();
+export default App;
